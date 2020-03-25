28eab0f93ce5b393466b6158b12ed078652ea5a2
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2020-03-25 09:00:01 +0300

    Adding file handle function Open.

--- Virgo/Include/FileHandle.h
+++ Virgo/Include/FileHandle.h
@@ -3,6 +3,7 @@
 #include "Handle.h"
 #include <string>
 #include <cstdint>
+#include <stdexcept>
 
 namespace Virgo
 {
@@ -12,6 +13,11 @@ namespace Virgo
 	class FileHandle : public Handle<FileHandle>
 	{
 	public:
+		explicit FileHandle(HANDLE& other)
+			: HandleType(other)
+		{
+		}
+
 		explicit FileHandle(const std::string& path, const std::string& mode)
 			: HandleType(FileHandle::Open(path, mode))
 		{
@@ -20,10 +26,54 @@ namespace Virgo
 		/*
 		 * Opens a file specified by path with specified access rights.
 		 */
-		static HandleType& Open(const std::string& path, const std::string& mode)
+		static const HandleType& Open(const std::string& path, const std::string& mode)
 		{
-			// TODO: Implement this function.
+			// Read file permission (r)
+			static constexpr const size_t FLAG_READ = 1;
+			// Write file permission (r)
+			static constexpr const size_t FLAG_WRITE = 2;
+			// Exclusive file flag (x)
+			static constexpr const size_t FLAG_EXCLUSIVE = 4;
 			
+			size_t flags{ 0 };
+			for (auto chr = mode.cbegin(); chr != mode.cend(); ++chr)
+			{
+				switch (*chr)
+				{
+				case 'r':
+					flags |= FLAG_READ;
+					break;
+				case 'w':
+					flags |= FLAG_WRITE;
+					break;
+				case 'x':
+					flags |= FLAG_EXCLUSIVE;
+					break;
+				default:
+					throw std::invalid_argument("Invalid mode string");
+				}
+			}
+
+			DWORD desiredAccess{ 0 };
+			DWORD fileShare{ 0 };
+
+			if (flags & FLAG_READ)
+				desiredAccess |= GENERIC_READ;
+			if (flags & FLAG_WRITE)
+				desiredAccess |= GENERIC_WRITE;
+			if (flags & FLAG_EXCLUSIVE)
+				fileShare = 0;
+			else
+				fileShare = FILE_SHARE_READ | FILE_SHARE_WRITE;
+
+			HANDLE hFile{ ::CreateFileA(path.c_str(), desiredAccess, fileShare, 
+				NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL) };
+			if (hFile == INVALID_HANDLE_VALUE)
+			{
+				throw NewException(::GetLastError());
+			}
+			
+			return FileHandle{ hFile };
 		}
 
 		/* 
