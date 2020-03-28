f8651786a52ab8ff124c2527da1d8c3787418b75
Buğra Kadirhan <54780023+bugravirgomercury@users.noreply.github.com>
2020-03-28 03:17:05 +0300

    Revert "Trying to fix FileHandle mess..."
    This reverts commit a9cea7f71080871ddead4316c4be2f6ed9a5cf91.

--- Virgo/Include/FileHandle.h
+++ Virgo/Include/FileHandle.h
@@ -48,47 +48,156 @@ namespace Virgo
 		/**
 		 * Interprets mode string into fileShare/desiredAccess pair.
 		 */
-		static const std::tuple<DWORD, DWORD> GetFileShareDesiredAccessPairFromModeString(const std::string& mode);
+		static const std::tuple<DWORD, DWORD> GetFileShareDesiredAccessPairFromModeString(const std::string& mode)
+		{
+			// Read file permission (r)
+			static constexpr const size_t FLAG_READ = 1;
+			// Write file permission (r)
+			static constexpr const size_t FLAG_WRITE = 2;
+			// Exclusive file flag (x)
+			static constexpr const size_t FLAG_EXCLUSIVE = 4;
+
+			size_t flags{ 0 };
+			for (auto chr = mode.cbegin(); chr != mode.cend(); ++chr)
+			{
+				switch (*chr)
+				{
+				case 'r':
+					flags |= FLAG_READ;
+					break;
+				case 'w':
+					flags |= FLAG_WRITE;
+					break;
+				case 'x':
+					flags |= FLAG_EXCLUSIVE;
+					break;
+				default:
+					throw std::invalid_argument("Invalid mode string");
+				}
+			}
+
+			DWORD desiredAccess{ 0 };
+			DWORD fileShare{ 0 };
+
+			if (flags & FLAG_READ)
+				desiredAccess |= GENERIC_READ;
+			if (flags & FLAG_WRITE)
+				desiredAccess |= GENERIC_WRITE;
+			if (flags & FLAG_EXCLUSIVE)
+				fileShare = 0;
+			else
+				fileShare = FILE_SHARE_READ | FILE_SHARE_WRITE;
+
+			return std::make_tuple(fileShare, desiredAccess);
+		}
 		
 
 		/**
 		 * Opens a file specified by path with specified access rights.
 		 */
-		static const HandleType& Open(const std::string& path, const std::string& mode);
+		static const HandleType& Open(const std::string& path, const std::string& mode)
+		{
+			DWORD desiredAccess{ 0 };
+			DWORD fileShare{ 0 };
+			std::tie(fileShare, desiredAccess) = GetFileShareDesiredAccessPairFromModeString(mode);
+
+			HANDLE hFile{ ::CreateFileA(path.c_str(), desiredAccess, fileShare, 
+				NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL) };
+			if (hFile == INVALID_HANDLE_VALUE)
+			{
+				throw NewException(::GetLastError());
+			}
+			
+			return FileHandle{ hFile };
+		}
 
 		/**
 		 * Writes the buffer into file.
 		 */
-		std::size_t Write(const void* buffer, std::size_t offsetInBuffer, std::size_t size);
+		std::size_t Write(const void* buffer, std::size_t offsetInBuffer, std::size_t size)
+		{
+			/*
+			 * NOTE: Try to refactor void* into something more C++.
+			 */
+
+			const void* offsetedBuffer{ static_cast<const char*>(buffer) + offsetInBuffer };
+			DWORD dwWritten{ 0 };
+			if (!::WriteFile(this, offsetedBuffer, size - offsetInBuffer, &dwWritten, NULL))
+				throw NewException(::GetLastError());
+			return dwWritten;
+		}
 
 		/**
 		 * Reads from file into buffer.
 		 */
-		std::size_t Read(void* buffer, std::size_t offsetInBuffer, std::size_t size);
+		std::size_t Read(void* buffer, std::size_t offsetInBuffer, std::size_t size)
+		{
+			/*
+			 * NOTE: Try to refactor void* into something more C++.
+			 */
+
+			void* offsetedBuffer{ static_cast<char*>(buffer) + offsetInBuffer };
+			DWORD dwRead{ 0 };
+			if (!::ReadFile(this, offsetedBuffer, size - offsetInBuffer, &dwRead, NULL))
+				throw NewException(::GetLastError());
+			return dwRead;
+		}
 
 		/**
 		 * Flushes the caches into disk.
 		 */
-		void Flush();
+		void Flush()
+		{
+			if (!::FlushFileBuffers(this))
+				throw NewException(::GetLastError());
+		}
 
 		/**
 		 * Seeks into specific offset in file.
 		 */
-		std::size_t Seek(std::int64_t offset, FileHandleWhence whence);
+		std::size_t Seek(std::int64_t offset, FileHandleWhence whence)
+		{
+			LARGE_INTEGER distanceToMove{ offset };
+			LARGE_INTEGER liOffset{ 0 };
+			DWORD moveMethod{ static_cast<DWORD>(whence) };
+			if (!::SetFilePointerEx(this, distanceToMove, &liOffset, moveMethod))
+				throw NewException(::GetLastError());
+			return static_cast<std::size_t>(liOffset.QuadPart);
+		}
 
 		/**
 		 * Tells the offset of file handle, relative to file beginning.
 		 */
-		std::size_t Tell();
+		std::size_t Tell()
+		{
+			return Seek(0, FileHandleWhence::FromCurrentPosition);
+		}
 
 		/**
 		 * Checks if end of file has been reached.
 		 */
-		bool IsEndOfFile();
+		bool IsEndOfFile()
+		{
+			char buffer[1];
+
+			try
+			{
+				Read(&buffer, 0, 1);
+				Seek(-1, FileHandleWhence::FromCurrentPosition);
+				return true;
+			}
+			catch (std::system_error const& x)
+			{
+				return false;
+			}
+		}
 
 		/**
 		 * Closes the file.
 		 */
-		void Close();
+		void Close()
+		{
+			::CloseHandle(this);
+		}
 	};
 }
--- Virgo/Include/Handle.h
+++ Virgo/Include/Handle.h
@@ -59,42 +59,91 @@ namespace Virgo
 		/*
 		 * Copy assignment.
 		 */
-		HandleType& operator=(const HandleType& other);
+		HandleType& operator=(const HandleType& other)
+		{
+			if (this != &other)
+			{
+				DupHandle(other.handle_, handle_);
+			}
+
+			return *this;
+		}
 
 		/*
 		 * Move assignment.
 		 */
-		HandleType& operator=(HandleType&& other);
+		HandleType& operator=(HandleType&& other)
+		{
+			if (this != &other)
+			{
+				if (handle_ != INVALID_HANDLE_VALUE)
+				{
+					// TODO: Check for result and throw exception accordingly.
+					other.Close();
+				}
+
+				handle_ = other.handle_;
+
+				other.handle_ = INVALID_HANDLE_VALUE;
+			}
+
+			return *this;
+		}
 
 		/*
 		 * Destructor
 		 */
-		virtual ~Handle();
+		virtual ~Handle()
+		{
+			try
+			{
+				Close();
+			}
+			catch (std::system_error const& ex)
+			{
+#ifdef _DEBUG
+				std::stringstream stream{};
+				stream << "Error occurred whilst closing handle - error #" << std::hex << ex.code();
+				OutputDebugStringA(static_cast<LPCSTR>(stream.str().c_str()));
+#endif
+			}
+		}
 
 		/**
 		 * Closes the underlying handle.
 		 */
-		void Close();
+		void Close()
+		{
+			static_cast<THandleImpl*>(this)->Close();
+		}
 
-		/**
+		/*
 		 * Conversion operator for getting handle value.
 		 */
-		operator HANDLE&() const;
-
-		/**
-		 * Get underlying handle.
-		 */
-		HANDLE& GetHandle();
-
-		/**
-		 * Get underlying handle (const)
-		 */
-		const HANDLE& GetHandle() const;
+		operator HANDLE&() const
+		{
+			return handle_;
+		}
 	private:
 		/**
 		 * Duplicates a handle to b handle.
 		 */
-		static void DupHandle(const HANDLE& a, HANDLE& b);
+		static void DupHandle(const HANDLE& a, HANDLE& b)
+		{
+			DWORD dwFlags{ 0 };
+			BOOL getHandleInformationResult{ ::GetHandleInformation(a, &dwFlags) };
+			BOOL duplicateHandleResult{ ::DuplicateHandle(
+				::GetCurrentProcess(), a,
+				::GetCurrentProcess(), &b, 0,
+				dwFlags & HANDLE_FLAG_INHERIT, DUPLICATE_SAME_ACCESS)
+			};
+
+			// TODO: Throw exception depending on GetLastError().
+			if (!duplicateHandleResult)
+			{
+				throw NewException(::GetLastError());
+			}
+		}
 
 		HANDLE handle_;
 	};
